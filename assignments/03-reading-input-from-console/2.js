73f3abcbe8f81eb8b72e9342a9aa20ac5f2ce373
DwiEkoRaharjo-SKom <dwieko.comp.cell@gmail.com>
2025-12-16 12:03:12 +0700

    Respon/Jawaban untuk Assignments 06, dan 07

--- assignments/03-reading-input-from-console/2.js
+++ assignments/03-reading-input-from-console/2.js
@@ -2,6 +2,6 @@ const readline = require("readline-sync");
 
 const miles = +readline.question("Enter miles: ");
 const MILES_TO_KM = 1.6093433333333333;
-const kilometers = miles * MILES_TO_KM
+const kilometers = miles * MILES_TO_KM;
 
 console.log("\n" + miles + " miles is equal to " + kilometers + " kilometers");
